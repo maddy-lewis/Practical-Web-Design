33e7b2b36fc7a9da1e237db91e263d1d12372c96
maddy-lewis <87465813+maddy-lewis@users.noreply.github.com>
2021-09-07 17:29:29 +1000

    Add files via upload

---
+++ home.js
@@ -0,0 +1,21 @@
+var app6 = new Vue({
+el: '#app-6',
+data: {
+     message: 'Type your message here'
+}
+})
+
+
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+        { text: 'Assingment 1: Project' },
+        { text: 'Assignment 2: Project and PPT' },
+        { text: 'Assessment 3: Portolfio' }
+        ]
+    }
+    })
+
+     
